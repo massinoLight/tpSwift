8df338e3d6ba12f10fbb0c4c934b6daf4b8cc61c
Massino <massino@Massinos-iMac.local>
2019-12-07 12:14:25 -0800

    Initial Commit

---
+++ RevisionTp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RevisionTp
+//
+//  Created by Massino on 07/12/2019.
+//  Copyright © 2019 Massino. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
